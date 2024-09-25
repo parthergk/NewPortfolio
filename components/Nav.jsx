75641ebd3949b83b1809b2d751dd9ed186cf3a40
gaurav5xy <gauravkumar81464@gmail.com>
2024-09-25 12:39:21 +0530

    NameChange

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,33 +1,11 @@
 "use client";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { memo, useState, useEffect } from "react";
+import { memo } from "react";
 import { menuSlide } from "@/lib/anim"; // Assuming menuSlide is defined in lib/anim
 
 const Nav = memo(({ isMobile, setIsActive }) => {
   const NavComponent = isMobile ? motion.nav : "nav";
-  const [deviceType, setDeviceType] = useState("desktop");
-
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      if (width < 640) {
-        setDeviceType("mobile");
-      } else if (width >= 640 && width < 1024) {
-        setDeviceType("tablet");
-      } else {
-        setDeviceType("desktop");
-      }
-    };
-
-    // Run on mount and on resize
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   // Calculate dynamic offset based on device height
   const getDynamicOffset = (sectionId) => {
@@ -74,9 +52,7 @@ const Nav = memo(({ isMobile, setIsActive }) => {
       exit={isMobile ? "exit" : undefined}
       className="bg-custom-gray sm:bg-transparent sm:relative sm:flex-none fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-10"
     >
-      <ul
-      className="sm:flex sm:space-x-5 text-white text-5.4xl sm:text-3.2xl lg:text-3.8xl font-light"
-      >
+      <ul className="sm:flex sm:space-x-5 text-white text-5.4xl sm:text-3.2xl lg:text-3.8xl font-light">
         {["Service", "Project", "About"].map((item) => (
           <li key={item}>
             <Link href={`/#${item}`} onClick={(e) => handleScroll(e, item)}>
